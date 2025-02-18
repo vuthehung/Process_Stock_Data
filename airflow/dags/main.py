53515e33f8e7c49c89202abe66a86eed5e1c1b8f
vuthehung <hungvt280102@gmail.com>
2025-02-18 17:07:20 +0700

    Demo data flow

---
+++ airflow/dags/main.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator # type: ignore
+from airflow.operators.python_operator import PythonOperator # type: ignore
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2025, 2, 18),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes = 5),
+}
+
+dag = DAG("main", default_args = default_args, schedule_interval = timedelta(30))
+
+def extract_load():
+    print(1)
+
+extract_load_task = PythonOperator(
+    task_id = "extract_load_task",
+    python_callable = extract_load,
+    dag = dag
+)
+
+transform_task = BashOperator(
+    task_id = "transform_task",
+    bash_command = 'spark-submit /opt/airflow/code/spark.py',
+    dag = dag
+)
+query_task = BashOperator(
+    task_id = "query_task",
+    bash_command = 'cd /opt/airflow/code && ./trino --server http://trino:8080 --file trino.sql',
+    dag = dag
+)
+
+extract_load_task >> transform_task >> query_task
